1aec8932f1694766356069b338ed3097cd184ec1
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-03-19 16:39:42 +0500

    create_zadacha_1

--- zadacha_1_seminar_3.py
+++ zadacha_1_seminar_3.py
@@ -1,1 +1,11 @@
+q = int(input("Введите натуральное число n: "))
 
+spisok = []
+
+for i in range(1, q + 1):
+    elem = input(f"Введте {i}-й элемент списка: ")
+    spisok.append(elem)
+
+n = input("Введите элемент списка: ")
+
+print(spisok.count(n))
